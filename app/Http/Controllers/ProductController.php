098791f5d8f7544e36ebbd13fc1e9a31be1fc12f
pprommitr <pprommitr@gmail.com>
2024-10-05 20:56:11 +0700

    up controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    //
+    // Function to show the product page
+    public function showProductPage($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return redirect()->back()->with('error', 'Product not found.');
+        }
+
+        
+        return view('product.show', compact('product'));
+    }
+    public function getAllProducts()
+    {
+        $products = Product::all();
+        return view('product.index', compact('products'));
+    }
+
 }
