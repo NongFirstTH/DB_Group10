f2846ad73ae16c3cf604267bdcfeb2082a5f725d
PoomNattapoom <rockyaggi@gmail.com>
2024-10-17 21:13:12 +0700

    change color to orange

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -39,7 +39,7 @@
             <p class="font-normal text-lg leading-8 text-gray-500 my-2 min-[550px]:my-3 max-[550px]:text-center">
               {{$item->category_name}}
             </p>
-            <h6 class="font-medium text-lg leading-8 text-indigo-600  max-[550px]:text-center">${{$item->price}}.00</h6>
+            <h6 class="font-medium text-lg leading-8 text-orange-600  max-[550px]:text-center">${{$item->price}}.00</h6>
           </div>
         </div>
         <div class="flex items-center flex-col min-[550px]:flex-row w-full max-xl:max-w-xl max-xl:mx-auto gap-2">
@@ -71,7 +71,7 @@
               </svg>
             </button>
           </div>
-          <h6 class="text-indigo-600 font-manrope font-bold text-2xl leading-9 w-full max-w-[176px] text-center">
+          <h6 class="text-orange-600 font-manrope font-bold text-2xl leading-9 w-full max-w-[176px] text-center">
             ${{$item->total_amount}}.00</h6>
         </div>
       </div>
@@ -87,26 +87,25 @@
           </h6>
         </div>
         <div class="flex items-center justify-between w-full pb-6 border-b border-gray-200">
-          <p class="font-normal text-xl leading-8 text-gray-400">Delivery Charge</p>
+          <p class="font-normal text-xl leading-8 text-gray-400">Discount</p>
           <h6 class="font-semibold text-xl leading-8 text-gray-900">$0.00</h6>
         </div>
         <div class="flex items-center justify-between w-full py-6">
           <p class="font-manrope font-medium text-2xl leading-9 text-gray-900">Total</p>
-          <h6 class="font-manrope font-medium text-2xl leading-9 text-indigo-500">${{$subtotal}}.00</h6>
+          <h6 class="font-manrope font-medium text-2xl leading-9 text-orange-500">${{$subtotal}}.00</h6>
         </div>
       </div>
       <div class="flex items-center flex-col sm:flex-row justify-center gap-3 mt-8">
         <button
-          class="rounded-full py-4 w-full max-w-[280px]  flex items-center bg-indigo-50 justify-center transition-all duration-500 hover:bg-indigo-100">
-          <span class="px-2 font-semibold text-lg leading-8 text-indigo-600">Add Coupon Code</span>
+          class="rounded-full py-4 w-full max-w-[280px]  flex items-center bg-orange-50 justify-center transition-all duration-500 hover:bg-orange-100">
+          <span class="px-2 font-semibold text-lg leading-8 text-orange-600">Add Coupon Code</span>
           <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
             <path d="M8.25324 5.49609L13.7535 10.9963L8.25 16.4998" stroke="#4F46E5" stroke-width="1.6"
               stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </button>
         <button
-          class="rounded-full w-full max-w-[280px] py-4 text-center justify-center items-center bg-indigo-600 font-semibold text-lg text-white flex transition-all duration-500 hover:bg-indigo-700">Continue
-          to Payment
+          class="rounded-full w-full max-w-[280px] py-4 text-center justify-center items-center bg-orange-600 font-semibold text-lg text-white flex transition-all duration-500 hover:bg-orange-700">Checkout
           <svg class="ml-2" xmlns="http://www.w3.org/2000/svg" width="23" height="22" viewBox="0 0 23 22" fill="none">
             <path d="M8.75324 5.49609L14.2535 10.9963L8.75 16.4998" stroke="white" stroke-width="1.6"
               stroke-linecap="round" stroke-linejoin="round" />
