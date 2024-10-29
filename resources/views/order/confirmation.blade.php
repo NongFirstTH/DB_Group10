f4f71a989f0c9a54335d1dbb7d37a065dbd2b7db
KonLew <surangrat231@gmail.com>
2024-10-29 21:52:02 +0700

    UP FROM nEUAH

--- resources/views/order/confirmation.blade.php
+++ resources/views/order/confirmation.blade.php
@@ -1,17 +1,29 @@
+@extends("layouts.layout")
+<div class="min-h-screen flex bg-gray-100">
+
+        <div class="mt-28 max-w-7xl mx-auto max-h-l">
 
 
-<div class="flex items-center justify-center min-h-screen bg-gray-100">
-  <div class="bg-white shadow-md rounded-lg p-6 max-w-md text-center">
-    <h1 class="text-2xl font-bold text-gray-800 mb-4">Order Confirmation</h1>
-    <p class="text-gray-600 mb-6">Your order has been placed successfully! Thank you for shopping with us.</p>
    
-    <div class="flex justify-center mt-6">
-      <button type="submit"  class="bg-gray-700 text-white px-6 py-3 rounded-md hover:bg-gray-800">
-          Back to Home
-      </button>
+      <div class="bg-white shadow-md rounded-lg p-6 max-w-md text-center"> 
+
+      <h1 class="text-5xl font-bold text-center ">CONFIRMATION</h1>
+        <p class="text-gray-600 mb-6">Your order has been placed successfully! Thank you for shopping with us.</p>
+      
+    
+        <!-- <div class="flex justify-between items-center mt-6"> -->
+          <button>
+            <a href="{{ route('cart.show') }}" class="text-lg underline text-gray-700 hover:text-gray-900">
+              {{ __('Back ') }}
+            </a>
+          </button>              
+        <!-- </div> -->
+
+
+       
+      </div>
+     </div>
     </div>
-
-
   </div>
 </div>
 
